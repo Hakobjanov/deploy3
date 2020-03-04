ecc5c93402fad83a8bdede54abb99af3fd630344
vahan akopjanian <vahan.bclub@gmail.com>
2020-03-04 16:44:49 +0300

    Updates

---
+++ precache-manifest.13b07faad7e23445e8e924c5f494dbe1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c694393a908f0d8568bf873bb693cfe",
+    "url": "/deploy3/index.html"
+  },
+  {
+    "revision": "f540c9c2e15a34362430",
+    "url": "/deploy3/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "596bcda711ecdba0834b",
+    "url": "/deploy3/static/js/2.82fc7ded.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/deploy3/static/js/2.82fc7ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f540c9c2e15a34362430",
+    "url": "/deploy3/static/js/main.5fe0fbf1.chunk.js"
+  },
+  {
+    "revision": "64fa718432f7301773d2",
+    "url": "/deploy3/static/js/runtime-main.57aac83a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy3/static/media/logo.5d5d9eef.svg"
+  }
+]);
